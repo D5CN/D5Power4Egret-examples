8f2bf12790861688578a0857fac81598896a9b7e
D5Power <D5PowerStudio@gmail.com>
2015-12-26 00:47:47 +0800

    新增D5BitmapUI界面初始化过程

--- D5PowerTemplate/D5PowerTemplate/src/Main.ts
+++ D5PowerTemplate/D5PowerTemplate/src/Main.ts
@@ -1,6 +1,90 @@
 class Main extends egret.Sprite {
 
+    /**
+     * 加载进度界面
+     * Process interface loading
+     */
+    private loadingView: LoadingUI;
+    
     public constructor() {
         super();
+        this.addEventListener(egret.Event.ADDED_TO_STAGE,this.onAddToStage,this);
+    }
+    
+    private onAddToStage(event: egret.Event) {
+        //设置加载进度界面
+        //Config to load process interface
+        this.loadingView = new LoadingUI();
+        this.stage.addChild(this.loadingView);
+
+        //加载预加载资源
+        RES.addEventListener(RES.ResourceEvent.CONFIG_COMPLETE,this.onConfigComplete,this);
+        RES.loadConfig("resource/default.res.json","resource/");
+    }
+    
+    /**
+     * 配置文件加载完成,开始预加载preload资源组。
+     * configuration file loading is completed, start to pre-load the preload resource group
+     */
+    private onConfigComplete(event: RES.ResourceEvent): void {
+        RES.removeEventListener(RES.ResourceEvent.CONFIG_COMPLETE,this.onConfigComplete,this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_COMPLETE,this.onResourceLoadComplete,this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR,this.onResourceLoadError,this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_PROGRESS,this.onResourceProgress,this);
+        RES.addEventListener(RES.ResourceEvent.ITEM_LOAD_ERROR,this.onItemLoadError,this);
+        RES.loadGroup("preload");
+    }
+    
+    /**
+     * preload资源组加载完成
+     * Preload resource group is loaded
+     */
+    private onResourceLoadComplete(event: RES.ResourceEvent): void {
+        if(event.groupName == "preload") {
+            this.stage.removeChild(this.loadingView);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_COMPLETE,this.onResourceLoadComplete,this);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR,this.onResourceLoadError,this);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_PROGRESS,this.onResourceProgress,this);
+            RES.removeEventListener(RES.ResourceEvent.ITEM_LOAD_ERROR,this.onItemLoadError,this);
+            this.start();
+        }
+    }
+
+    /**
+     * 资源组加载出错
+     *  The resource group loading failed
+     */
+    private onItemLoadError(event: RES.ResourceEvent): void {
+        console.warn("Url:" + event.resItem.url + " has failed to load");
+    }
+
+    /**
+     * 资源组加载出错
+     *  The resource group loading failed
+     */
+    private onResourceLoadError(event: RES.ResourceEvent): void {
+        //TODO
+        console.warn("Group:" + event.groupName + " has failed to load");
+        //忽略加载失败的项目
+        //Ignore the loading failed projects
+        this.onResourceLoadComplete(event);
+    }
+
+    /**
+     * preload资源组加载进度
+     * Loading process of preload resource group
+     */
+    private onResourceProgress(event: RES.ResourceEvent): void {
+        if(event.groupName == "preload") {
+            this.loadingView.setProgress(event.itemsLoaded,event.itemsTotal);
+        }
+    }
+    
+    private start():void
+    {
+        // 初始化D5BitmapUI界面风格
+        d5power.D5UIResourceData.setupResLib(<egret.Texture><any>RES.getRes('ui_texture'),RES.getRes('ui_config'));
+        
+
     }
 }
